02713a4ee8a527103d0a6d191f080cb4ced6218a
Ramil Assanov <aramille@googlemail.com>
2025-04-15 22:22:32 +0200

    added captcha v2 func

--- domain/email.go
+++ domain/email.go
@@ -1,65 +1,48 @@
 package domain
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 	"os"
 	"reverse-job-board/internal"
-	"strings"
 )
 
 func SendNewEmail(templateId, userId, receiverEmail string, code int) error {
 	internal.LogInfo("Starting sending sign up confirmation email", map[string]interface{}{"user_id": userId})
-
-	// Get Mailgun credentials from environment variables
-	mailgunDomain := os.Getenv("MAILGUN_DOMAIN")
-	mailgunAPIKey := os.Getenv("MAILGUN_API_KEY")
-
-	if mailgunDomain == "" || mailgunAPIKey == "" {
-		// For development: just log the verification link and return success
-		internal.LogInfo("Mailgun credentials not found, skipping actual email send", map[string]interface{}{
+	mailTrapToken := os.Getenv("MAILTRAP_TOKEN")
+	requestBody := map[string]interface{}{
+		"from": map[string]string{
+			"email": "hello@angulartalents.com",
+			"name":  "The Angular Team",
+		},
+		"to": []interface{}{
+			map[string]string{"email": receiverEmail},
+		},
+		"template_uuid": templateId,
+		"template_variables": map[string]string{
 			"user_id":           userId,
-			"verification_link": fmt.Sprintf("http://localhost:8080/verify/%s/%d", userId, code),
-			"email":             receiverEmail,
-		})
-		return nil
+			"verification_code": fmt.Sprint(code),
+		},
 	}
 
-	// The verification URL that will be linked in the email
-	verificationURL := fmt.Sprintf("https://angulartalents.onrender.com/verify/%s/%d", userId, code)
-
-	// Build form data for the Mailgun API
-	formData := url.Values{}
-	formData.Add("from", "The Angular Team <hello@angulartalents.com>")
-	formData.Add("to", receiverEmail)
-	formData.Add("subject", "Verify Your Angular Talents Account")
-
-	emailHTML := fmt.Sprintf(`
-		<h2>Welcome to Angular Talents!</h2>
-		<p>Thank you for signing up. Please verify your email address by clicking the link below:</p>
-		<p><a href="%s">Verify Your Email</a></p>
-		<p>Or copy and paste this URL into your browser:</p>
-		<p>%s</p>
-		<p>If you did not create an account, you can safely ignore this email.</p>
-		<p>Best regards,<br>The Angular Team</p>
-	`, verificationURL, verificationURL)
-
-	formData.Add("html", emailHTML)
-
-	// Create the request
 	client := &http.Client{}
-	mailgunURL := fmt.Sprintf("https://api.mailgun.net/v3/%s/messages", mailgunDomain)
+	url := "https://send.api.mailtrap.io/api/send"
 
-	req, err := http.NewRequest("POST", mailgunURL, strings.NewReader(formData.Encode()))
+	marshalledRequestBody, err := json.Marshal(requestBody)
 	if err != nil {
 		return err
 	}
 
-	req.SetBasicAuth("api", mailgunAPIKey)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(marshalledRequestBody))
+	if err != nil {
+		return err
+	}
 
-	// Send the request
+	req.Header.Add("Authorization", "Bearer "+mailTrapToken)
+	req.Header.Add("Content-Type", "application/json")
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
@@ -67,7 +50,7 @@ func SendNewEmail(templateId, userId, receiverEmail string, code int) error {
 
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		err := internal.NewError(http.StatusInternalServerError, "email.send", "request to Mailgun API failed", fmt.Sprintf("failed to send email, status code: %d", resp.StatusCode))
+		err := internal.NewError(http.StatusInternalServerError, "email.send", "request to mailtrap api failed", "failed to send email")
 		internal.LogError(err, internal.ReturnRawData(resp))
 		return err
 	}
