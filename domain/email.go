704429edc2ce3ded27d8f9df90864a6c090037ec
Ramil Assanov <aramille@googlemail.com>
2025-04-25 17:22:20 +0200

    add recruiter email approval handler

--- domain/email.go
+++ domain/email.go
@@ -58,3 +58,122 @@ func SendNewEmail(templateId, userId, receiverEmail string, code int) error {
 	internal.LogInfo("Successfully sent sign up confirmation email", map[string]interface{}{"user_id": userId})
 	return nil
 }
+
+// SendRecruiterApprovalEmail sends an email to a recruiter when their profile is approved
+func SendRecruiterApprovalEmail(recruiterID, firstName, lastName, company, receiverEmail string) error {
+	internal.LogInfo("Starting to send recruiter approval email", map[string]interface{}{"recruiter_id": recruiterID})
+
+	mailTrapToken := os.Getenv("MAILTRAP_TOKEN")
+	approvalTemplateID := os.Getenv("RECRUITER_APPROVAL_TEMPLATE_ID")
+
+	if approvalTemplateID == "" {
+		internal.LogInfo("Recruiter approval template ID not set, using fallback method", nil)
+		return sendFallbackApprovalEmail(receiverEmail, firstName, company)
+	}
+
+	requestBody := map[string]interface{}{
+		"from": map[string]string{
+			"email": "hello@angulartalents.com",
+			"name":  "Angular Talents",
+		},
+		"to": []interface{}{
+			map[string]string{"email": receiverEmail},
+		},
+		"template_uuid": approvalTemplateID,
+		"template_variables": map[string]string{
+			"first_name":   firstName,
+			"last_name":    lastName,
+			"company":      company,
+			"login_url":    fmt.Sprintf("%s/login", os.Getenv("FRONTEND_URL")),
+			"recruiter_id": recruiterID,
+		},
+	}
+
+	client := &http.Client{}
+	url := "https://send.api.mailtrap.io/api/send"
+
+	marshalledRequestBody, err := json.Marshal(requestBody)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(marshalledRequestBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+mailTrapToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		err := internal.NewError(http.StatusInternalServerError, "email.send_approval", "request to mailtrap api failed", "failed to send approval email")
+		internal.LogError(err, internal.ReturnRawData(resp))
+		return err
+	}
+
+	internal.LogInfo("Successfully sent recruiter approval email", map[string]interface{}{"recruiter_id": recruiterID})
+	return nil
+}
+
+// sendFallbackApprovalEmail sends a basic approval email when template ID is not configured
+func sendFallbackApprovalEmail(receiverEmail, firstName, company string) error {
+	mailTrapToken := os.Getenv("MAILTRAP_TOKEN")
+
+	// Simple subject and text for fallback email
+	subject := "Your Angular Talents Business Profile has been approved!"
+	text := fmt.Sprintf("Hello %s,\n\nGreat news! Your business profile for %s has been approved. You now have full access to the Angular Talents platform and can view all engineers' profiles.\n\nLog in now to start connecting with top Angular talent: %s/login\n\nBest regards,\nThe Angular Talents Team",
+		firstName,
+		company,
+		os.Getenv("FRONTEND_URL"),
+	)
+
+	// Build the request body for a simple email without a template
+	requestBody := map[string]interface{}{
+		"from": map[string]string{
+			"email": "hello@angulartalents.com",
+			"name":  "Angular Talents",
+		},
+		"to": []interface{}{
+			map[string]string{"email": receiverEmail},
+		},
+		"subject": subject,
+		"text":    text,
+	}
+
+	client := &http.Client{}
+	url := "https://send.api.mailtrap.io/api/send"
+
+	marshalledRequestBody, err := json.Marshal(requestBody)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(marshalledRequestBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+mailTrapToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		err := internal.NewError(http.StatusInternalServerError, "email.send_fallback_approval", "request to mailtrap api failed", "failed to send fallback approval email")
+		internal.LogError(err, internal.ReturnRawData(resp))
+		return err
+	}
+
+	internal.LogInfo("Successfully sent fallback recruiter approval email", map[string]interface{}{"email": receiverEmail})
+	return nil
+}
--- handlers/recruiter-update-status.go
+++ handlers/recruiter-update-status.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // UpdateRecruiterStatusRequest represents the request to update a recruiter's status
@@ -22,7 +23,7 @@ func HandleRecruiterUpdateStatus(w internal.EnhancedResponseWriter, r *internal.
 	recruiterID := vars["recruiterID"]
 
 	// Validate UUID
-	_, err := uuid.Parse(recruiterID)
+	parsedID, err := uuid.Parse(recruiterID)
 	if err != nil {
 		return internal.NewError(
 			http.StatusBadRequest,
@@ -43,16 +44,35 @@ func HandleRecruiterUpdateStatus(w internal.EnhancedResponseWriter, r *internal.
 		)
 	}
 
+	// Fetch the recruiter to get their details
+	recruiterCol := db.Database.Collection("recruiters")
+
+	// Get the recruiter before updating it
+	var recruiter domain.Recruiter
+	err = recruiterCol.FindOne(
+		r.Context(),
+		bson.M{"_id": parsedID},
+	).Decode(&recruiter)
+
+	if err != nil {
+		return internal.NewError(
+			http.StatusInternalServerError,
+			"recruiter.update_status.find_failed",
+			"Failed to find recruiter",
+			err.Error(),
+		)
+	}
+
+	// Store the previous status to detect status change
+	wasApproved := recruiter.IsMember
+
 	// Create a custom update payload since `is_member` is not in the standard UpdateRecruiterPayload
 	// We'll use a map to update only the is_member field
 	updateData := map[string]interface{}{
 		"is_member": req.IsMember,
 	}
 
-	// Use FindOneAndUpdate directly in handler or modify dao function to accept a map
-	recruiterCol := db.Database.Collection("recruiters")
-	parsedID, _ := uuid.Parse(recruiterID)
-
+	// Update the recruiter status
 	var updatedRecruiter domain.Recruiter
 	err = recruiterCol.FindOneAndUpdate(
 		r.Context(),
@@ -67,6 +87,44 @@ func HandleRecruiterUpdateStatus(w internal.EnhancedResponseWriter, r *internal.
 			"Failed to update recruiter status",
 			err.Error(),
 		)
+	}
+
+	// If the status changed from not approved to approved, send an email
+	if !wasApproved && req.IsMember {
+		// Get the associated user to retrieve the email
+		usersCol := db.Database.Collection("users")
+		var user domain.User
+
+		err = usersCol.FindOne(
+			r.Context(),
+			bson.M{"_id": recruiter.UserID},
+		).Decode(&user)
+
+		if err != nil {
+			internal.LogInfo("Failed to find user for approved recruiter notification", map[string]interface{}{
+				"recruiter_id": recruiterID,
+				"user_id":      recruiter.UserID.String(),
+				"error":        err.Error(),
+			})
+		} else {
+			// Send email notification asynchronously
+			go func() {
+				err := domain.SendRecruiterApprovalEmail(
+					recruiterID,
+					recruiter.Firstname,
+					recruiter.Lastname,
+					recruiter.Company,
+					user.Email,
+				)
+
+				if err != nil {
+					internal.LogInfo("Failed to send approval email", map[string]interface{}{
+						"recruiter_id": recruiterID,
+						"error":        err.Error(),
+					})
+				}
+			}()
+		}
 	}
 
 	internal.LogInfo("Successfully updated recruiter status", map[string]interface{}{
