1166b8dd383a5f4a1173a0663c693737b1a9e3c9
Ramil Assanov <aramille@googlemail.com>
2025-05-04 21:50:10 +0200

    Update email address from Mailtrap to production domain in SMTP test script and email handling functions

--- domain/email.go
+++ domain/email.go
@@ -49,7 +49,7 @@ func SendNewEmail(templateId, userId, receiverEmail string, code int) error {
 	verificationLink := fmt.Sprintf("%s/verify/%s/%d", frontendURL, userId, code)
 
 	// Set up email data
-	from := "hello@angular-talents.mailtrap.io"
+	from := "hello@angulartalents.com"
 	to := []string{receiverEmail}
 
 	// Create email headers
@@ -167,7 +167,7 @@ func SendRecruiterApprovalEmail(recruiterID, firstName, lastName, company, recei
 	loginURL := fmt.Sprintf("%s/login", frontendURL)
 
 	// Set up email data
-	from := "hello@angular-talents.mailtrap.io"
+	from := "hello@angulartalents.com"
 	to := []string{receiverEmail}
 
 	// Create email headers
